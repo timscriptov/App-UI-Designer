e9f8459e22922a5c29da6d3f12f361ee6f3c8d09
Иван Тимашков <timscriptov@mail.ru>
2023-03-13 00:42:53 +0500

    Optimization sort layout list

--- app/src/main/java/com/mcal/example/utils/Utils.kt
+++ app/src/main/java/com/mcal/example/utils/Utils.kt
@@ -4,16 +4,9 @@ import com.mcal.example.data.Const
 
 object Utils {
     fun isAndroidXLayout(layoutName: String): Boolean {
-        val regex =
-            Regex("""^int (\S+) (\S+)\s+([\dx]+)$|^int (\S+) (\S+) (\S+)\s+([\dx]+)$|^int (\S+) (\S+) (\S+) (\S+)\s+([\dx]+)$""")
-
-        Const.layout.forEach { line ->
-            val matchResult = regex.find(line)
-            if (matchResult != null) {
-                val name = matchResult.groupValues[2]
-                if (layoutName.contains(name)) {
-                    return true
-                }
+        Const.layout.forEach { name ->
+            if (layoutName.contains(name)) {
+                return true
             }
         }
         return false
